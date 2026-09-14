6b11868123f8f36c45333cffc0b89ddd80540314
Hemapriya <hemapriyatk007@gmail.com>
2026-09-14 21:30:24 +0530

    Time: 2 ms (71.94%), Space: 45 MB (92.97%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int res[]=new int[prices.length];
+        int k=0;
+        for(int i=0;i<prices.length;i++){
+            if(i==prices.length-1){
+                res[k]=prices[i];
+                k++;
+            }
+            for(int j=i+1;j<prices.length;j++){
+                if(prices[i]>=prices[j]){
+                  res[k]=prices[i]-prices[j];
+                  k++;
+                  break;
+        
+                }
+                 else if(j==prices.length-1){
+                    res[k]=prices[i];
+                    k++;
+                    break;
+                 }   
+                
+            }
+            
+           
+                    
+            
+        }
+        return res;
+        
+    }
+}
