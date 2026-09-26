77d4740779b9bed4f4c73877a59bf20dacc5c344
Hemapriya <hemapriyatk007@gmail.com>
2026-09-26 22:03:03 +0530

    Time: 5 ms (3.31%), Space: 44 MB (5.08%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        String s1="";
+        String s2="";
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='#'){
+                if(s1.length()>0){
+                s1=s1.substring(0,s1.length()-1);
+                
+            }
+            }
+            else{
+                s1+=ch;
+            }
+        }
+        for(int j=0;j<t.length();j++){
+            char ch1=t.charAt(j);
+            
+            if(ch1=='#'){
+                if(s2.length()>0){
+                s2=s2.substring(0,s2.length()-1);
+
+            }
+            }
+            else{
+                s2+=ch1;
+            }
+        }
+        if(s1.equals(s2)){
+            return true;
+        }
+        return false;
+        
+    }
+}
